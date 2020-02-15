97511c09710c509e97009d4d364f5a33dccbda04
Tlaster <tlaster@outlook.com>
2020-02-15 17:23:41 +0800

    Add localization

---
+++ RockChat.Core/Common/ActionMessageHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RockChat.Core.i18n;
+using Rocket.Chat.Net.Models;
+
+namespace RockChat.Core.Common
+{
+    public static class ActionMessageHelper
+    {
+        public static string GetMessage(MessageData data)
+        {
+            var msg = data.Msg;
+            var username = data.User.UserName;
+            var role = data.Role;
+
+            var message = data.Type switch
+            {
+                "rm" => Localization.Message_removed,
+                "uj" => Localization.Has_joined_the_channel,
+                "ut" => Localization.Has_joined_the_conversation,
+                "r" => string.Format(Localization.Room_name_changed, msg, username),
+                "message_pinned" => Localization.Message_pinned,
+                "jitsi_call_started" => string.Format(Localization.Started_call, username),
+                "ul" => Localization.Has_left_the_channel,
+                "ru" => string.Format(Localization.User_removed_by, msg, username),
+                "au" => string.Format(Localization.User_added_by, msg, username),
+                "user-muted" => string.Format(Localization.User_muted_by, msg, username),
+                "user-unmuted" => string.Format(Localization.User_unmuted_by, msg, username),
+                "subscription-role-added" => string.Format(Localization.User_role_add, msg, role, username),
+                "subscription-role-removed" => string.Format(Localization.User_role_remove, msg, role, username),
+                "room_changed_description" => string.Format(Localization.Room_changed_description, msg, username),
+                "room_changed_announcement" => string.Format(Localization.Room_changed_announcement, msg, username),
+                "room_changed_topic" => string.Format(Localization.Room_changed_topic, msg, username),
+                "room_changed_privacy" => string.Format(Localization.Room_changed_privacy, msg, username),
+                "message_snippeted" => Localization.Created_snippet,
+                _ => string.Empty,
+            };
+
+            return message;
+        }
+    }
+}
--- RockChat.UWP/Common/ActionMessageConverter.cs
+++ RockChat.UWP/Common/ActionMessageConverter.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
+using RockChat.Core.Common;
 using Rocket.Chat.Net.Models;
 
 namespace RockChat.UWP.Common
@@ -17,35 +18,9 @@ namespace RockChat.UWP.Common
                 return string.Empty;
             }
 
-            var msg = data.Msg;
-            var username = data.User.UserName;
-            var role = data.Role;
-
-            var message = data.Type switch
-            {
-                "rm" => "Message_removed",
-                "uj" => "Has_joined_the_channel",
-                "ut" => "Has_joined_the_conversation",
-                "r" => string.Format("Room_name_changed", msg, username),
-                "message_pinned" => "Message_pinned",
-                "jitsi_call_started" => string.Format("Started_call", username),
-                "ul" => "Has_left_the_channel",
-                "ru" => string.Format("User_removed_by", msg, username),
-                "au" => string.Format("User_added_by", msg, username),
-                "user-muted" => string.Format("User_muted_by", msg, username),
-                "user-unmuted" => string.Format("User_unmuted_by", msg, username),
-                "subscription-role-added" => string.Format("User_role_add", msg, role, username),
-                "subscription-role-removed" => string.Format("User_role_remove", msg, role, username),
-                "room_changed_description" => string.Format("Room_changed_description", msg, username),
-                "room_changed_announcement" => string.Format("Room_changed_announcement", msg, username),
-                "room_changed_topic" => string.Format("Room_changed_topic", msg, username),
-                "room_changed_privacy" => string.Format("Room_changed_privacy", msg, username),
-                "message_snippeted" => "Created_snippet",
-                _ => string.Empty,
-            };
-
-            return message;
+            return ActionMessageHelper.GetMessage(data);
         }
+
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
